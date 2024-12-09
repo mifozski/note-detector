e47017c584886303d97271eebce49f911275b3fa
Andrey Efimov <mifozski@gmail.com>
2024-12-09 20:07:48 +0300

    init

---
+++ devserver.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    publicPaths: {
+        // '/assets': 'assets'
+    },
+    app: '/index.html', // this can also be an express app
+    serverConfig: {}, // config for webpack-hot-server-middleware
+    port: 3000, // 3000 by default
+};
---
+++ index.js
@@ -0,0 +1,6 @@
+const wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: '#4F4A85',
+    progressColor: '#383351',
+    url: '/audio.mp3',
+  })
